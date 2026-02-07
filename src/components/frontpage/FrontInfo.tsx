5c4082aea99741ef36985cc2c43f3c3ccd8b63a7
Adele Xiao Yuan Strysse <axstryss@stud.ntnu.no>
2026-02-07 15:01:31 +0100

    style: fix placement of frontinfo
    fix placement on frontinfo

--- src/components/frontpage/FrontInfo.tsx
+++ src/components/frontpage/FrontInfo.tsx
@@ -5,9 +5,9 @@ export default function FrontInfo() {
   return (
     <div className="relative bg-[#1a4b36] w-full min-h-[40rem] md:min-h-screen flex flex-col justify-between">
       {/* Velkommen */}
-      <div className="flex flex-1 items-center justify-center text-center text-white font-bold text-4xl px-4">
-        <div>
-          <span className="block mt-20 md:mt-80">Velkommen til Autobank!</span>
+      <div className="flex flex-1 items-end justify-center text-center text-white font-bold text-4xl px-4 xl:pb-10 lg:pb-36 md:pb-72 pb-32">
+        <div className="">
+          <span className="block mt-20 md:text-5xl">Velkommen til Autobank!</span>
           <span className="block text-2xl mt-4">
             Her kan du legge inn kvitteringer for utlegg
           </span>
