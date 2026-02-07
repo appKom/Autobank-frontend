618bd3dff4a6e6bae7b32ec53021af06617a6d11
johngothe <117284605+johngothe@users.noreply.github.com>
2026-02-07 15:42:10 +0100

    style: adjust text placement frontinfo

--- src/components/frontpage/FrontInfo.tsx
+++ src/components/frontpage/FrontInfo.tsx
@@ -3,11 +3,11 @@ import bottomwave from "../../resources/frontpage/wave.svg";
 
 export default function FrontInfo() {
   return (
-    <div className="relative bg-[#1a4b36] w-full min-h-[40rem] md:min-h-screen flex flex-col justify-between">
+    <div className="relative bg-[#1a4b36] w-full min-h-[40rem] md:min-h-screen">
       {/* Velkommen */}
-      <div className="flex flex-1 items-end justify-center text-center text-white font-bold text-4xl px-4 xl:pb-10 lg:pb-36 md:pb-72 pb-32">
-        <div className="">
-          <span className="block mt-20 md:text-5xl">Velkommen til Autobank!</span>
+      <div className="absolute inset-0 flex items-center justify-center text-center text-white font-bold text-4xl px-4">
+        <div>
+          <span className="block md:text-5xl">Velkommen til Autobank!</span>
           <span className="block text-2xl mt-4">
             Her kan du legge inn kvitteringer for utlegg
           </span>
@@ -15,8 +15,7 @@ export default function FrontInfo() {
       </div>
 
       {/* BOTTOM WAVE */}
-      <img src={bottomwave} alt="" className="w-full block" />
-
+      <img src={bottomwave} alt="" className="w-full block absolute bottom-0" />
     </div>
   );
 }
