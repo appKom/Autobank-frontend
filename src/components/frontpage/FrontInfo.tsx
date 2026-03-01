8cb232ef8f8e73a68a5bdc940a0954363224ff85
Dina Marie Stenrud <dinamariestenrud@dhcp-10-22-67-158.wlan.ntnu.no>
2026-03-01 16:00:29 +0100

    feat: add money on the front page

--- src/components/frontpage/FrontInfo.tsx
+++ src/components/frontpage/FrontInfo.tsx
@@ -1,5 +1,7 @@
-import topwave from "../../resources/frontpage/topwave.svg";
+
 import bottomwave from "../../resources/frontpage/wave.svg";
+import moneyLeft from "../../resources/frontpage/money_left.png";
+import moneyRight from "../../resources/frontpage/money_right.png";
 
 export default function FrontInfo() {
   return (
@@ -10,9 +12,24 @@ export default function FrontInfo() {
           <span className="block md:text-5xl">Velkommen til Autobank!</span>
           <span className="block text-2xl mt-4">
             Her kan du legge inn kvitteringer for utlegg
-          </span>
+          </span>     
+
         </div>
       </div>
+
+      <img 
+        src={moneyRight} 
+        alt="" 
+        className="money-money object-contain w-[900px] h-auto absolute top-[20%] hidden xl:block
+                  right-[-50px] xl:right-[-200px]"/>
+      
+      <div className="absolute top-[25%] hidden xl:block left-[-200px]">
+        <img src={moneyLeft} alt="" className="money-money object-contain w-[900px] h-auto " />
+      </div>
+
+
+
+  
 
       {/* BOTTOM WAVE */}
       <img src={bottomwave} alt="" className="w-full block absolute bottom-0" />
